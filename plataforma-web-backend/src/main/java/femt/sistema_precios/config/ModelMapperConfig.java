28141354b77d6e346130f12b35943e638b3592f6
MenesesTech <menesesfrey@gmail.com>
2025-06-24 19:31:40 -0500

    Configuracion de ModelMapper agregado

--- plataforma-web-backend/src/main/java/femt/sistema_precios/config/ModelMapperConfig.java
+++ plataforma-web-backend/src/main/java/femt/sistema_precios/config/ModelMapperConfig.java
@@ -1,4 +1,11 @@
 package femt.sistema_precios.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
 public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
