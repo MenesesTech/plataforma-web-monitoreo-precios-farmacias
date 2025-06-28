e0f36bd00f55d945cfe18efa893a5056f2c1d8d0
MenesesTech <menesesfrey@gmail.com>
2025-06-28 08:55:59 -0500

    Habilitar propiedades de configuracion para ScraperApiConfig

--- plataforma-web-backend/src/main/java/femt/sistema_precios/SistemaPreciosApplication.java
+++ plataforma-web-backend/src/main/java/femt/sistema_precios/SistemaPreciosApplication.java
@@ -2,8 +2,12 @@ package femt.sistema_precios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import femt.sistema_precios.config.ScraperApiConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ScraperApiConfig.class)
 public class SistemaPreciosApplication {
 
 	public static void main(String[] args) {
