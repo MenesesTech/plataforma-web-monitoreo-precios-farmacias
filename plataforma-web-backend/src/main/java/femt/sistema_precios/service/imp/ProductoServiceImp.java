2bb1acffa5a7f4866690b169d937003f6456e08d
MenesesTech <menesesfrey@gmail.com>
2025-07-01 10:46:52 -0500

    ProductoService y su implementacion para listar productos con mejor precio

---
+++ plataforma-web-backend/src/main/java/femt/sistema_precios/service/imp/ProductoServiceImp.java
@@ -0,0 +1,36 @@
+package femt.sistema_precios.service.imp;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import femt.sistema_precios.dto.ProductoCardDTO;
+import femt.sistema_precios.mapper.ProductoMapper;
+import femt.sistema_precios.model.Producto;
+import femt.sistema_precios.repository.ProductoRepository;
+import femt.sistema_precios.service.ProductoService;
+
+@Service
+public class ProductoServiceImp implements ProductoService {
+
+    private final ProductoRepository productoRepository;
+    private final ProductoMapper productoMapper;
+
+    public ProductoServiceImp(ProductoRepository productoRepository, ProductoMapper productoMapper) {
+        this.productoRepository = productoRepository;
+        this.productoMapper = productoMapper;
+    }
+
+    @Override
+    public List<ProductoCardDTO> listarProductosConMejorPrecio() throws Exception {
+        try {
+            List<Producto> productos = productoRepository.findAll();
+            return productos.stream()
+                    .map(productoMapper::toCardDTO)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new Exception("Error al obtener productos con mejor precio: " + e.getMessage());
+        }
+    }
+}
